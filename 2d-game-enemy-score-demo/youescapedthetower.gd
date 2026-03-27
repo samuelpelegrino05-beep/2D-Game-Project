eec6204a310b6749ca1431f6b925393029bed79f
Izz McKee <imckee@fordham.edu>
2026-03-26 22:00:22 -0400

    consolidation

--- 2d-game-enemy-score-demo/youescapedthetower.gd
+++ 2d-game-enemy-score-demo/youescapedthetower.gd
@@ -1,11 +1,10 @@
 extends Control
 
-@export var next_scene: String = "res://youescapedthetower.gd"
+@export var you_escaped_the_tower: String = "res://You escaped the tower.tscn"
 
 func _on_button_pressed():
 	GameManager.key_found.clear()
 	GameManager.opened_doors.clear()
 	GameManager.visited.clear()
 	
-	# Reload the game
-	get_tree().change_scene_to_file(next_scene)
+	get_tree().change_scene_to_file(you_escaped_the_tower)
